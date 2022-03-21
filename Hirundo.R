d29464868b87a823b621f6baa097a42a727feeca
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-03-21 16:11:16 +0100

    added averaging

--- Hirundo.R
+++ Hirundo.R
@@ -1,67 +1,136 @@
 library(rgbif)
 library(ggplot2)
+library(ggtext)
+library(tidyverse)
+library(data.table)
 library(RColorBrewer)  # for brewer.pal(...)
 
 map <- map_data("france")
 
+dup_obs <- function(occs){
+  occs$individualCount[is.na(occs$individualCount)] <- round(mean(occs$individualCount, na.rm=TRUE))
+  idx <- rep(1:nrow(occs), occs$individualCount)
+  
+  return(occs[idx,])
+}
+
 make_maps <- function(occs, year=""){
   locations <- as.data.frame(occs)
-  locations <- locations[,c("acceptedScientificName","stateProvince", "year",
-                            "recordedBy", "basisOfRecord",
-                            "decimalLatitude","decimalLongitude")]
   
   ggplot(locations, aes(x=decimalLongitude, y=decimalLatitude)) + 
-    stat_density2d(aes(fill = ..level..), alpha=0.5, geom="polygon")+
-    geom_path(data=map,aes(x=long, y=lat,group=group), colour="grey50")+
-    scale_fill_gradientn(colours=rev(brewer.pal(7,"Spectral")))+
+    stat_density2d(aes(fill = ..level..), alpha=0.5, geom="polygon") +
+    geom_path(data=map, aes(x=long, y=lat,group=group), colour="grey50") +
+    scale_fill_gradientn(colours=rev(brewer.pal(7,"Spectral"))) +
     coord_fixed() +
     ggtitle(paste("Heatmap de Hirundo rustica pour la décennie", year))
   
-  ggsave(paste0("./maps/hirundo_heatmap_", year, ".png"))
+  ggsave(paste0("./maps/hirundo/hirundo_heatmap_", year, ".png"))
 }
 
-# occs_1970 <- occ_search(scientificName="Hirundo rustica", limit=50000, country='FR', year="1970,1979", hasCoordinate=TRUE)
-occs_1970 <- import("./data/Hirundo_rustica_1970/occurrence.txt", header=TRUE, sep="\t")
-# occs_2010 <- occ_search(scientificName="Hirundo rustica", limit=50000, country='FR', year="2010,2019", hasCoordinate=TRUE)
-occs_2010 <- import("./data/Hirundo_rustica_2010/occurrence.txt", header=TRUE, sep="\t")
 
-make_maps(occs_1970$data, "1970")
-make_maps(occs_2010$data, "2010")
+to_keep <- c("gbifID", "identifier", "occurrenceID", "catalogNumber",
+             "recordedBy", "individualCount", "occurrenceStatus", "eventDate",
+             "year", "month", "day", "countryCode", "stateProvince", "county",
+             "municipality", "decimalLatitude", "decimalLongitude", "datasetKey",
+             "hasCoordinate", "hasGeospatialIssues", "level0Gid", "level0Name",
+             "level1Gid", "level1Name", "level2Gid", "level2Name", "level3Gid",
+             "level3Name", "iucnRedListCategory")
+
+# occs_1970 <- occ_search(scientificName="Hirundo rustica", limit=50000, country='FR', year="1970,1979", hasCoordinate=TRUE)
+occs_1970 <- data.table::fread("./data/Hirundo_rustica_1970/occurrence.txt",
+                               data.table=FALSE,
+                               fill=FALSE,
+                               encoding="UTF-8",
+                               select=to_keep) %>%
+  filter(!hasGeospatialIssues & hasCoordinate & occurrenceStatus=="PRESENT") %>%
+  select(-c(hasGeospatialIssues, hasCoordinate, occurrenceStatus)) %>%
+  dup_obs
+
+# occs_2010 <- occ_search(scientificName="Hirundo rustica", limit=50000, country='FR', year="2010,2019", hasCoordinate=TRUE)
+occs_2010 <- data.table::fread("./data/Hirundo_rustica_2010/occurrence.txt",
+                               data.table=FALSE,
+                               fill=FALSE,
+                               encoding="UTF-8",
+                               select=to_keep) %>%
+  filter(!hasGeospatialIssues & hasCoordinate & occurrenceStatus=="PRESENT") %>%
+  select(-c(hasGeospatialIssues, hasCoordinate, occurrenceStatus)) %>%
+  dup_obs
+
+make_maps(occs_1970, "1970")
+make_maps(occs_2010, "2010")
+
+# moyennage ----
+
+mean_map <- function(occs, decennie, level){
+  mean_locations <- occs %>%
+    group_by_("eventDate", paste0("level", level, "Gid")) %>%
+    summarise_at(vars("decimalLongitude", "decimalLatitude"), mean)
+  
+  levels = c("régionales", "départementales", "communales")
+  
+  ggplot(mean_locations, aes(x=decimalLongitude, y=decimalLatitude)) + 
+    stat_density2d(aes(fill = ..level..), alpha=0.5, geom="polygon") +
+    geom_path(data=map, aes(x=long, y=lat,group=group), colour="grey50") +
+    scale_fill_gradientn(colours=rev(brewer.pal(7,"Spectral"))) +
+    coord_fixed() +
+    ggtitle(paste("Heatmap de moyennes", levels[level],
+                  "\nde Hirundo rustica pour la décennie", decennie))
+  
+  ggsave(paste0("./maps/hirundo/hirundo_mean_level_", levels[level], "_", decennie, ".png"))
+}
+
+mean_map(occs_1970, "1970", 1)
+mean_map(occs_1970, "1970", 2)
+mean_map(occs_1970, "1970", 3)
+mean_map(occs_2010, "2010", 1)
+mean_map(occs_2010, "2010", 2)
+mean_map(occs_2010, "2010", 3)
 
 # ----
 
-library(tidyverse)
+# library(maptools)
+# data("wrld_simpl")
+# SP <- SpatialPoints(coords=mean_locations[c("decimalLongitude","decimalLatitude")],
+#                       proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
+# plot(SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16)
+# plot(wrld_simpl, add=TRUE)
+# 
+# for (key in unique(locations$datasetKey)){
+#   SP <- SpatialPoints(coords=locations[locations$datasetKey==key,],
+#                       proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
+#   mean_locations <- locations[locations$datasetKey==key,] %>%
+#     summarise_at(vars("decimalLongitude", "decimalLatitude"), mean)
+#   mean_SP <- SpatialPoints(coords=mean_locations,
+#                       proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
+#   plot(SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16)
+#   plot(mean_SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16, add=TRUE)
+#   plot(wrld_simpl, add=TRUE)
+# }
 
-locations <- as.data.frame(occs_1970$data)
 
-mean_locations <- locations %>%
-  group_by(datasetKey) %>%
-  summarise_at(vars("decimalLongitude", "decimalLatitude"), mean)
+# normalization ----
 
-geom_sf(data=map) +
-  geom_sf(data=mean_locations, color=rgb(0.2, 0.2, 0.2, alpha=0.4)) +
-  theme(
-    panel.background = element_rect(fill = "#FFFFFF", colour = "#000000",
-                                    size = 1, linetype = "solid"),
-    panel.grid.major = element_line(size = 0.2, linetype = "solid",
-                                    colour = "#808080")
-  )
+total_1970 <- occ_count(country="FR", year="1970,1979")
+total_2010 <- occ_count(country="FR", year="2010,2019")
 
-library(maptools)
-data("wrld_simpl")
-SP <- SpatialPoints(coords=mean_locations[c("decimalLongitude","decimalLatitude")],
-                      proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
-plot(SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16)
-plot(wrld_simpl, add=TRUE)
 
-for (key in unique(locations$datasetKey)){
-  SP <- SpatialPoints(coords=locations[locations$datasetKey==key,],
-                      proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
-  mean_locations <- locations[locations$datasetKey==key,] %>%
-    summarise_at(vars("decimalLongitude", "decimalLatitude"), mean)
-  mean_SP <- SpatialPoints(coords=mean_locations,
-                      proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
-  plot(SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16)
-  plot(mean_SP, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16, add=TRUE)
-  plot(wrld_simpl, add=TRUE)
-}
+picus_key <- name_backbone(name="Picus viridis", rank="species")$speciesKey
+total_picus_1970 <- occ_count(taxonKey=picus_key, country="FR", year="1970,1979")
+total_picus_2010 <- occ_count(taxonKey=picus_key, country="FR", year="2010,2019")
+
+occs_1970 %>%
+  group_by(level2Name) %>% count(level2Name) %>% mutate(n = n / total_picus_1970) %>% filter(n > 2e-1) %>%
+  ggplot(aes(x = reorder(level2Name, n), y = n, fill = n)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  labs(x = "Region of Organism", y = "Number of Occurrence") + 
+  coord_flip()
+
+occs_2010 %>%
+  group_by(level2Gid) %>% count(level2Name) %>% mutate(n = n / total_picus_2010) %>% filter(n > 2e-1) %>%
+  ggplot(aes(x = reorder(level2Name, n), y = n, fill = n)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  labs(x = "Region of Organism", y = "Number of Occurrence") + 
+  coord_flip()
+
+
+
--- Picus_europe.R
+++ Picus_europe.R
@@ -3,15 +3,26 @@ rm(list=ls())
 library(rgbif)
 library(raster)
 library(ggplot2)
-library(sf)
+library(tidyverse)
+library(data.table)
+# library(sf)
 library(RColorBrewer)
 
-library(rnaturalearth)
-library(rnaturalearthdata)
-library(ggspatial)
+# library(rnaturalearth)
+# library(rnaturalearthdata)
+# library(ggspatial)
 
-world <- ne_countries(scale = "medium", returnclass = "sf")
-europe <- ne_countries(continent="europe", scale = "medium", returnclass = "sf")
+# world <- ne_countries(scale = "medium", returnclass = "sf")
+# europe <- ne_countries(continent="europe", scale = "medium", returnclass = "sf")
+france <- map_data("france")
+
+# duplicate by number of observation
+dup_obs <- function(occs){
+  occs$individualCount[is.na(occs$individualCount)] <- round(mean(occs$individualCount, na.rm=TRUE))
+  idx <- rep(1:nrow(occs), occs$individualCount)
+  
+  return(occs[idx,])
+}
 
 make_maps <- function(occs, year=""){
   locations <- as.data.frame(occs)
@@ -54,14 +65,84 @@ make_maps <- function(occs, year=""){
     ) +
     ggtitle(paste("Heatmap de Picus viridis pour l'année", year))
   
-  ggsave(paste0("./maps/picus_heatmap_", year, ".png"))
+  ggsave(paste0("./maps/picus/picus_heatmap_", year, ".png"))
 }
 
-occs_recent <- occ_search(scientificName="Picus viridis", limit=5000) # might reduce limit
+# occs_recent <- occ_search(scientificName="Picus viridis", limit=5000)
 
-occs_1970 <- occ_search(scientificName="Picus viridis", limit=5000, year="1970", hasCoordinate=TRUE)
-occs_2010 <- occ_search(scientificName="Picus viridis", limit=5000, year="2010", hasCoordinate=TRUE)
+to_keep <- c("gbifID", "identifier", "occurrenceID", "catalogNumber",
+             "recordedBy", "individualCount", "occurrenceStatus", "eventDate",
+             "year", "month", "day", "countryCode", "stateProvince", "county",
+             "municipality", "decimalLatitude", "decimalLongitude", "datasetKey",
+             "hasCoordinate", "hasGeospatialIssues", "level0Gid", "level0Name",
+             "level1Gid", "level1Name", "level2Gid", "level2Name", "level3Gid",
+             "level3Name", "iucnRedListCategory")
 
-make_maps(occs_1970$data, "1970")
-make_maps(occs_2010$data, "2010")
+# occs_1970 <- occ_search(scientificName="Picus viridis", limit=5000, year="1970,1979", hasCoordinate=TRUE)
+occs_1970 <- fread("./data/Picus_viridis_1970/occurrence.txt",
+                   data.table=FALSE,
+                   fill=FALSE,
+                   encoding="UTF-8",
+                   select=to_keep) %>%
+  filter(!hasGeospatialIssues & hasCoordinate & occurrenceStatus=="PRESENT") %>%
+  select(-c(hasGeospatialIssues, hasCoordinate, occurrenceStatus)) %>%
+  dup_obs()
 
+# occs_2010 <- occ_search(scientificName="Picus viridis", limit=5000, year="2010,2019", hasCoordinate=TRUE)
+occs_2010 <- fread("./data/Picus_viridis_2010/occurrence.txt",
+                   data.table=FALSE,
+                   fill=FALSE,
+                   encoding="UTF-8",
+                   select=to_keep) %>%
+  filter(!hasGeospatialIssues & hasCoordinate & occurrenceStatus=="PRESENT") %>%
+  select(-c(hasGeospatialIssues, hasCoordinate, occurrenceStatus)) %>%
+  dup_obs()
+
+make_maps(occs_1970, "1970")
+make_maps(occs_2010, "2010")
+
+# moyennage ----
+
+mean_map <- function(occs, decennie, level){
+  mean_locations <- occs %>%
+    group_by_("eventDate", paste0("level", level, "Gid")) %>%
+    summarise_at(vars("decimalLongitude", "decimalLatitude"), mean)
+  
+  levels = c("régionales", "départementales", "communales")
+  
+  ggplot(mean_locations, aes(x=decimalLongitude, y=decimalLatitude)) + 
+    stat_density2d(aes(fill = ..level..), alpha=0.5, geom="polygon") +
+    geom_path(data=france, aes(x=long, y=lat,group=group), colour="grey50") +
+    scale_fill_gradientn(colours=rev(brewer.pal(7,"Spectral"))) +
+    coord_fixed() +
+    ggtitle(paste("Heatmap de moyennes", levels[level],
+                  "\nde Picus viridis pour la décennie", decennie))
+  
+  ggsave(paste0("./maps/picus/picus_mean_", levels[level], "_", decennie, ".png"))
+}
+
+mean_map(occs_1970, "1970", 1)
+mean_map(occs_1970, "1970", 2)
+mean_map(occs_1970, "1970", 3)
+mean_map(occs_2010, "2010", 1)
+mean_map(occs_2010, "2010", 2)
+mean_map(occs_2010, "2010", 3)
+
+# normalization ----
+
+total_1970 <- occ_count(country="FR", year="1970,1979")
+total_2010 <- occ_count(country="FR", year="2010,2019")
+
+occs_1970 %>%
+  group_by(level2Name) %>% count(level2Name) %>% mutate(n = n / total_1970) %>% filter(n > 1e-6) %>%
+  ggplot(aes(x = reorder(level2Name, n), y = n, fill = n)) +
+    geom_bar(stat = "identity", show.legend = FALSE) +
+    labs(x = "Region of Organism", y = "Number of Occurrence") + 
+    coord_flip()
+
+occs_2010 %>%
+  group_by(level2Gid) %>% count(level2Name) %>% mutate(n = n / total_2010) %>% filter(n > 1e-6) %>%
+  ggplot(aes(x = reorder(level2Name, n), y = n, fill = n)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  labs(x = "Region of Organism", y = "Number of Occurrence") + 
+  coord_flip()
