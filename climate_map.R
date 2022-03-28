14a18c715f2c57dfa8dbe30e8cd7292fd86e8ebd
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-03-28 18:25:34 +0200

    improved preprocessing of maps

--- climate_map.R
+++ climate_map.R
@@ -1,6 +1,5 @@
 library(raster)
 library(tidyverse)
-library(viridis)
 
 
 france <- getData('GADM', country='FR', level=2)
@@ -32,50 +31,84 @@ files_1970 <- list.files(paste0(inputDir, "/1970"),
 
 # precipitation for first month of 1970
 precipRaster <- raster(grep(files_1970, pattern='_prec_', value=TRUE)[1])
-precipRaster <- crop(precipRaster, bbox(poly))
+precipRaster <- crop(precipRaster, poly)
 precipRaster <- mask(precipRaster, poly)
 
 plot(precipRaster, legend = FALSE)
 
-# preprocess all rasters
-for (i in 1:length(files_1970)) {
-  raster(files_1970[i]) %>% 
-    crop(poly) %>% 
-    mask(poly) %>% 
-    aggregate(fact=2) %>%
-    writeRaster(filename=sub("^(.*[/])",
-                             paste0(outputDir, "/1970/"),
-                             files_1970[i]),
-                format="GTiff",
-                overwrite=TRUE,
-                options=c("COMPRESS=DEFLATE",
-                          "PREDICTOR=2",
-                          "ZLEVEL=6"))
-}
 
-# create raster layer for mean temperature
+# complete preprocessing route ----
 
-tmin_stack <- list.files(paste0(outputDir, "/1970"),
-                         pattern='tmin',
-                         full.names=TRUE) %>% stack()
+tmin_stack_1970 <- grep(files_1970, pattern='_tmin_', value=TRUE) %>% 
+  stack() %>% 
+  crop(poly) %>% 
+  mask(poly) %>%
+  aggregate(fact=2)
 
-tmax_stack <- list.files(paste0(outputDir, "/1970"),
-                         pattern='tmax',
-                         full.names=TRUE) %>% stack()
+tmax_stack_1970 <- grep(files_1970, pattern='_tmax_', value=TRUE) %>% 
+  stack() %>% 
+  crop(poly) %>% 
+  mask(poly)
 
-prec_stack <- list.files(paste0(outputDir, "/1970"),
-                         pattern='prec',
-                         full.names=TRUE) %>% stack()
+prec_stack_1970 <- grep(files_1970, pattern='_prec_', value=TRUE) %>% 
+  stack() %>% 
+  crop(poly) %>% 
+  mask(poly)
 
-tmin_mean <- calc(tmin_stack, mean)
-tmax_mean <- calc(tmax_stack, mean)
-tmean = (tmin_mean + tmax_mean) / 2
-prec_mean <- calc(prec_stack, mean)
+tmin_mean_1970 <- calc(tmin_stack_1970, mean)
+tmax_mean_1970 <- calc(tmax_stack_1970, mean)
+tmean_1970 = (tmin_mean_1970 + tmax_mean_1970) / 2
+prec_mean_1970 <- calc(prec_stack_1970, mean)
 
-par(mfrow=c(2, 2))
-plot(tmin_mean, main="minimal temperatures")
-plot(tmax_mean, main="maximal temperatures")
-plot(tmean, main="average temperatures")
-plot(prec_mean, main="average precipiration")
-par(mfrow=c(1, 1))
+writeRaster(filename=paste0(outputDir, "/1970/avg_prec_1970.tif"),
+            format="GTiff",
+            overwrite=TRUE,
+            options=c("COMPRESS=DEFLATE",
+                      "PREDICTOR=2",
+                      "ZLEVEL=6"))
+
+# preprocess all rasters ----
+
+# for (i in 1:length(files_1970)) {
+#   raster(files_1970[i]) %>% 
+#     crop(poly) %>% 
+#     mask(poly) %>% 
+#     aggregate(fact=2) %>%
+#     writeRaster(filename=sub("^(.*[/])",
+#                              paste0(outputDir, "/1970/"),
+#                              files_1970[i]),
+#                 format="GTiff",
+#                 overwrite=TRUE,
+#                 options=c("COMPRESS=DEFLATE",
+#                           "PREDICTOR=2",
+#                           "ZLEVEL=6"))
+# }
+
+# averaging raster layers ----
+
+# tmin_stack_1970 <- list.files(paste0(outputDir, "/1970"),
+#                               pattern='tmin',
+#                               full.names=TRUE) %>% stack()
+# 
+# tmax_stack_1970 <- list.files(paste0(outputDir, "/1970"),
+#                               pattern='tmax',
+#                               full.names=TRUE) %>% stack()
+# 
+# prec_stack_1970 <- list.files(paste0(outputDir, "/1970"),
+#                               pattern='prec',
+#                               full.names=TRUE) %>% stack()
+# 
+# tmin_mean_1970 <- calc(tmin_stack_1970, mean)
+# tmax_mean_1970 <- calc(tmax_stack_1970, mean)
+# tmean_1970 = (tmin_mean_1970 + tmax_mean_1970) / 2
+# prec_mean_1970 <- calc(prec_stack_1970, mean)
+
+
+# par(mfrow=c(2, 2))
+# plot(tmin_mean, main="minimal temperatures")
+# plot(tmax_mean, main="maximal temperatures")
+# plot(tmean, main="average temperatures")
+# plot(prec_mean, main="average precipiration")
+# par(mfrow=c(1, 1))
+
 
