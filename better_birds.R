d3a045ba3fb3b7032e03962f405e6f49dbcad63a
Baptiste Rousseau <bapt09.rousseau@gmail.com>
2022-04-04 14:16:35 +0200

    improved code readability

--- better_birds.R
+++ better_birds.R
@@ -18,7 +18,7 @@
 #       - Picus viridis
 
 
-rm(list=ls())
+rm(list = ls())
 
 library(tidyverse)
 library(rgbif)
@@ -29,57 +29,53 @@ library(sf)
 library(maptools)
 data("wrld_simpl")
 
-get_birds <- function(specie_name, plot_occ=FALSE, plot_heat=FALSE){
-  occs <- occ_search(scientificName=specie_name, limit=5000, hasCoordinate=TRUE)
-  if (plot_occ){
+get_birds <- function(specie_name, plot_occ = FALSE, plot_heat = FALSE) {
+  occs <- occ_search(scientificName = specie_name,
+                     limit = 5000,
+                     hasCoordinate = TRUE)
+  if (plot_occ) {
     plot_occurences(occs)
   }
-  
-  if (plot_heat){
-    plot_heatmap(specie_name) 
+  if (plot_heat) {
+    plot_heatmap(specie_name)
   }
-  
   return(occs$data)
 }
 
 
 # plot occurence points
-plot_occurences <- function(occs){
+plot_occurences <- function(occs) {
   locations <- as.data.frame(occs$data)
-  locations <- locations[,c("acceptedScientificName","stateProvince", "year",
-                            "recordedBy", "basisOfRecord", "references",
-                            "decimalLatitude","decimalLongitude")]
-  occs <- SpatialPoints(coords=locations[c("decimalLongitude","decimalLatitude")],
-                        proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
-  plot(occs, col=rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch=16)
-  plot(wrld_simpl, add=TRUE)
+  locations <- locations[, c("acceptedScientificName", "stateProvince", "year",
+                             "recordedBy", "basisOfRecord", "references",
+                             "decimalLatitude", "decimalLongitude")]
+  occs <- SpatialPoints(coords = locations[c("decimalLongitude", "decimalLatitude")],
+                        proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "))
+  plot(occs, col = rgb(red = 0.2, green = 0.2, blue = 0.2, alpha = 0.4), pch = 16)
+  plot(wrld_simpl, add = TRUE)
 }
 
 # plot heatmap
-plot_heatmap <- function(specie_name){
-  specie_key <- name_backbone(name=specie_name, rank="species")$speciesKey
-  
-  map <- map_fetch(taxonKey=specie_key,
-                   format="@4x.png",
-                   srs="EPSG:3857",       # SRS for whole world, 3575 for Europe
-                   bin="hex",
-                   hexPerTile=60,
-                   style="purpleYellow.poly")
+plot_heatmap <- function(specie_name) {
+  specie_key <- name_backbone(name = specie_name, rank = "species")$speciesKey
+  map <- map_fetch(taxonKey = specie_key,
+                   format = "@4x.png",
+                   srs = "EPSG:3857",   # SRS for whole world, 3575 for Europe
+                   bin = "hex",
+                   hexPerTile = 60,
+                   style = "purpleYellow.poly")
   plot(map)
 }
 
 
-Hirundo.occs <- get_birds("Hirundo rustica", T, T)
-Picus.occs <- get_birds("Picus viridis", T, T)
+hirundo_occs <- get_birds("Hirundo rustica", T, T)
+picus_occs <- get_birds("Picus viridis", T, T)
 
-Picus.occs %>% count(stateProvince, sort = TRUE) %>% drop_na(stateProvince)  %>% filter(n > 50) %>%
+picus_occs %>%
+  count(stateProvince, sort = TRUE) %>%
+  drop_na(stateProvince) %>%
+  filter(n > 50) %>%
   ggplot(aes(x = reorder(stateProvince, n), y = n, fill = n)) +
   geom_bar(stat = "identity", show.legend = FALSE) +
-  labs(x = "Country of Organism", y = "Number of Occurrence") + 
+  labs(x = "Country of Organism", y = "Number of Occurrence") +
   coord_flip()
-
-# Picus.occs %>% drop_na(year) %>% count(year) %>%      # un peu osef
-#   ggplot(aes(x = year, y = n, group = 1)) +
-#   geom_line()
-
-
--- download.R
+++ download.R
@@ -1,52 +1,56 @@
 library(rgbif)
 
-download_GBIF <- function(specie_name, decade, n_try=30, Sys.sleep_duration=30){
+download_gbif <- function(specie_name, decade, n_try=30, sleep_duration=30) {
   ref <- paste0(gsub(" ", "_", specie_name), "_", as.character(decade))
-  
-  specie_key <- name_backbone(name=specie_name, rank="species")$speciesKey
-  
+  specie_key <- name_backbone(name = specie_name, rank = "species")$speciesKey
+
   start_time <- Sys.time()
   download_key <- occ_download(
     pred("taxonKey", specie_key),
-    pred("country", 'FR'),
+    pred("country", "FR"),
     pred("hasCoordinate", TRUE),
     pred_and(
       pred_gte("year", decade),
       pred_lt("year", decade + 10)
     )
   )
-  
+
   download_url <- paste0("http://api.gbif.org/v1/occurrence/download/request/",
                         download_key[1], ".zip")
-  
-  try_download <- try(download.file(url=download_url,
-                                    destfile=paste0("./data/", ref, ".zip"),
-                                    mode="wb",
-                                    quiet=TRUE),
-                      silent=TRUE)
-  
+
+  try_download <- try(download.file(url = download_url,
+                                    destfile = paste0("./data/", ref, ".zip"),
+                                    mode = "wb",
+                                    quiet = TRUE),
+                      silent = TRUE)
+
   n <- 1
   while (inherits(try_download, "try-error") & n < n_try) {
-    print(paste("Le lien de téléchargement n'est pas encore prêt. Temps écoulé (min) :",
-                round(as.numeric(paste(difftime(Sys.time(),start_time, units = "mins"))),2)))
-    Sys.sleep(Sys.sleep_duration)
-    
-    try_download <- try(download.file(url=download_url,
-                                      destfile=paste0("./data/", ref, ".zip"),
-                                      mode="wb",
-                                      quiet=TRUE),
-                        silent=TRUE)
+    print(paste("Le lien de telechargement n'est pas encore pret.",
+                "Temps ecoule (min) :",
+                round(as.numeric(paste(difftime(Sys.time(),
+                                                start_time,
+                                                units = "mins"))), 2)))
+    Sys.sleep(sleep_duration)
+
+    try_download <- try(download.file(url = download_url,
+                                      destfile = paste0("./data/", ref, ".zip"),
+                                      mode = "wb",
+                                      quiet = TRUE),
+                        silent = TRUE)
     n <- n + 1
-    if (n == n_try){
-      stop("Le jeu de donnée n'a pas pu être téléchargé, vérifiez les arguments et réassayez plus tard")
+    if (n == n_try) {
+      stop("Le jeu de donnee n'a pas pu etre telecharge, verifiez les arguments et reassayez plus tard")
     }
   }
-  unzip(paste0("./data/",ref, ".zip"), exdir=paste0("./data/", ref), overwrite=TRUE)
-  unlink(paste0("./data/",ref, ".zip"))
+  unzip(paste0("./data/", ref, ".zip"),
+        exdir = paste0("./data/", ref),
+        overwrite = TRUE)
+  unlink(paste0("./data/", ref, ".zip"))
 }
 
-download_GBIF("Hirundo rustica", 1970)
-download_GBIF("Hirundo rustica", 2010)
+download_gbif("Hirundo rustica", 1970)
+download_gbif("Hirundo rustica", 2010)
 
-download_GBIF("Picus viridis", 1970)
-download_GBIF("Picus viridis", 2010)
+download_gbif("Picus viridis", 1970)
+download_gbif("Picus viridis", 2010)
