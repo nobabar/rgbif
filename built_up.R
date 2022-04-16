013da15b6c6e2dbbce3f3890e9d7b83d52a45dc8
babar <bapt09.rousseau@gmail.com>
2022-04-16 20:49:00 +0200

    preprocess ghs built up data

---
+++ built_up.R
@@ -0,0 +1,32 @@
+library(raster)
+library(tidyverse)
+
+france <- shapefile("./data/gadm40_FRA_shp/gadm40_FRA_2.shp")
+
+built_up_1970 <- raster("./data/built_up_land/1970/GHS_BUILT_LDS1975_GLOBE_R2018A_54009_1K_V2_0.tif") %>%
+  crop(., spTransform(france, crs(.))) %>%
+  mask(., spTransform(france, crs(.))) %>%
+  writeRaster("./data/built_up_land/1970/GHS_BUILT_1975_FRANCE.tif")
+
+
+built_up_2010 <- raster("./data/built_up_land/2010/GHS_BUILT_LDS2014_GLOBE_R2018A_54009_1K_V2_0.tif") %>%
+  crop(., spTransform(france, crs(.))) %>%
+  mask(., spTransform(france, crs(.))) %>%
+  writeRaster("./data/built_up_land/2010/GHS_BUILT_2014_FRANCE.tif")
+
+
+# 18th century roads and cities data ----
+
+# library(cowplot)
+
+# edge <- shapefile("./data/cassini_18th/edge/edge.shp")
+# face <- shapefile("./data/cassini_18th/face/face.shp")
+# cities <- shapefile("./data/cassini_18th/france_cassini_cities/france_cassini_cities.shp")
+# roads <- shapefile("./data/cassini_18th/france_cassini_roads/france_cassini_roads.shp")
+# 
+# edge_plot <- ggplot() + geom_polygon(data = edge, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
+# face_plot <- ggplot() + geom_polygon(data = face, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
+# cities_plot <- ggplot() + geom_polygon(data = cities, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
+# roads_plot <- ggplot() + geom_polygon(data = roads, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
+# 
+# plot_grid(edge_plot, face_plot, cities_plot, roads_plot)
