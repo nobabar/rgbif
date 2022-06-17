cfabc3b6e3b1cd42aaee46606120a2926c79b774
babar <bapt09.rousseau@gmail.com>
2022-06-18 01:39:57 +0200

    final preprocessing pathway for rivers

--- preprocess_rivers.R
+++ preprocess_rivers.R
@@ -1,29 +1,77 @@
 library(sf)
 library(raster)
 
-
+# river shape ----
 rivers_shape <- list.files("./data/rivers/rivers_shape/", pattern = "gpkg$",
                            recursive = TRUE, full.names = TRUE)
 
-data <- st_read("./data/rivers/rivers_shape/euhydro_duero_v013_GPKG/euhydro_duero_v013.gpkg",
-                query='SELECT OBJECT_ID, AREA_GEO, Shape FROM River_Net_p')
-plot(st_zm(data$Shape))
+# read only Loire
+river_p <- st_read("./data/rivers/rivers_shape//euhydro_loire_v013_GPKG/euhydro_loire_v013.gpkg",
+                   query='SELECT OBJECT_ID, AREA_GEO, Shape FROM River_Net_p')
 
+plot(st_zm(river_p$Shape))
 
 layers <- st_layers("./data/rivers/rivers_shape/euhydro_duero_v013_GPKG/euhydro_duero_v013.gpkg")
-
-data.spatial <- as(st_zm(data), 'Spatial')
-
+data.spatial <- as(st_zm(river_p), 'Spatial')
 data.sf <- st_as_sf(data.spatial, "sf")
 
-riparian_files_2012 <- list.files("./data/rivers/riparian_zones/2012/", pattern = "shp$",
+
+# riparian zones ----
+france_tiles <- raster("./data/rivers/riparian_zones/france_tiles.tif")
+tiles_data <- france_tiles %>%
+  as("SpatialPixelsDataFrame") %>%
+  as.data.frame()
+
+interest_rivers = c("Loire"="rpz_DU017A", "Ebro"="rpz_DU019A")
+
+riparian_zone_stats <- list.files("./data/rivers/riparian_zones", pattern = "gpkg$",
                                   recursive = TRUE, full.names = TRUE)
 
-riparian_zones_2012 <- rbind(lapply(riparian_files_2012, st_read))
+# only select Loire
+riparian_zone_stats <- grep(interest_rivers["Loire"],
+                            riparian_zone_stats,
+                            value = TRUE)
 
-riparian_longlat <- st_transform(riparian_zones_2012,
-                                 "+proj=longlat +datum=WGS84 +no_defs")
+for (riparian_zone in riparian_zone_stats){
+  stats <- st_read(riparian_zone)
+  stats <- stats[order(stats$zone),]
+  
+  tdata <- tiles_data[,2:3]
+  tdata$value[tiles_data$france_tiles %in% stats$zone] <- stats$sum
+  
+  rdf <- rasterFromXYZ(tdata)
+  crs(rdf) <- crs(france_tiles)
+  
+  path <- gsub("(/[^/]*){3}$", "",
+               sub("(.*\\/)([^.]+)(\\.[[:alnum:]]+$)", "\\1", riparian_zone))
+  
+  filename <- gsub("(_[^_]*){2}$", "",
+                   sub("(.*\\/)([^.]+)(\\.[[:alnum:]]+$)", "\\2", riparian_zone))
+  
+  zone <- sub("(^([^_]*_){2})(.+)(_stats)", "\\3",
+               sub("(.*\\/)([^.]+)(\\.[[:alnum:]]+$)", "\\2", riparian_zone))
+  
+  river <- names(interest_rivers[interest_rivers==filename])
+  
+  writeRaster(rdf,
+              paste0(path, "/", river, "_", zone, ".tif"),
+              overwrite=TRUE,
+              format = "GTiff")
+}
 
-ext <- extent(riparian_longlat)
-rr <- raster(ext, res=0.005)
-rr <- rasterize(riparian_longlat, rr, field=riparian_longlat$CODE_1_12)
+
+
+# create simulacrae layer ----
+zfile <- raster("./data/BioClim/2000/bio1.tif")
+zdata <- zfile %>%
+  as("SpatialPixelsDataFrame") %>%
+  as.data.frame()
+
+zdata$bio1 <- NULL
+zdata$value <- seq_len(nrow(zdata))
+
+rdf <- rasterFromXYZ(zdata)
+crs(rdf) <- crs(zfile)
+writeRaster(rdf, "./data/rivers/riparian_zones/france_tiles.tif",
+            overwrite=TRUE,
+            format = "GTiff")
--- preprocess_rivers.py
+++ preprocess_rivers.py
@@ -4,9 +4,9 @@ either use directly in qgis console
 or use in python (commented part, not implemented yet)
 '''
 
-import processing
+import os
 import glob
-from pathlib import Path
+import processing
 
 # extract layer from raster
 
@@ -15,7 +15,6 @@ def calculator(in_file, out_file, expression):
     processing.run('qgis:rastercalculator', {'INPUT': in_file,
                                              'CELLSIZE': 100,
                                              'EXPRESSION': expression,
-                                             # from qgis.core
                                              'CRS': QgsCoordinateReferenceSystem('EPSG:3035'),
                                              'EXTENT': '3240269.854700000,3909569.854700000,2408574.042600000,2944074.042600000 [EPSG:3035]',
                                              'OUTPUT': out_file})
@@ -28,6 +27,17 @@ def zonal_stat(in_file, zone_file, out_file):
                                                   'ZONES': zone_file,
                                                   'ZONES_BAND': 1,
                                                   'OUTPUT_TABLE': out_file})
+
+# reproject a layer
+
+
+def reproject(in_file, out_file=None, crs='EPSG:4326'):
+    if out_file is None:
+        out_file = in_file.replace(".tif", "_reprojected.tif")
+    processing.run('gdal:warpreproject', {"INPUT": in_file,
+                                          "TARGET_CRS": QgsCoordinateReferenceSystem(crs),
+                                          "RESAMPLING": 1,
+                                          "OUTPUT": out_file})
 
 
 '''
@@ -47,15 +57,14 @@ qgs.initQgis()
 
 # Write your code here to load some layers, use processing
 # algorithms, etc.
-year = 2012
+year = 2010
 
 # list rivers shapefiles
-shapefiles = [Path(file).as_posix() for file in glob.glob(
-    f"./data/rivers/riparian_zones/{year}/*/*/*.shp")]
+shapefiles = glob.glob(f"./data/rivers/riparian_zones/{year}/*/*/*.shp")
 
 # convert to vector layers
 for shapefile in shapefiles:
-    outfile = shapefile.replace('.shp', '.tif')
+    rasterized = shapefile.replace('.shp', '.tif')
     # vlayer = QgsVectorLayer(shapefile, "", "ogr")
     processing.run('gdal:rasterize', {"INPUT": shapefile,
                                       "FIELD": "CODE_1_12",
@@ -63,11 +72,11 @@ for shapefile in shapefiles:
                                       "WIDTH": 100.0,
                                       "HEIGHT": 100.0,
                                       "DATA_TYPE": 1,
-                                      "OUTPUT": outfile})
+                                      "OUTPUT": rasterized})
 
 # list raster layers files
-tif_files = [Path(file).as_posix() for file in glob.glob(
-    f"./data/rivers/riparian_zones/{year}/*/*/*.tif")]
+projected_tif = glob.glob(
+    f"./data/rivers/riparian_zones/{year}/*/*/*.tif")
 
 # merge raster layers
 # outfile = f"./data/rivers/riparian_zones/{year}/riparian_zones.tif"
@@ -77,21 +86,26 @@ tif_files = [Path(file).as_posix() for file in glob.glob(
 
 interest_rivers = ["rpz_DU017A", "rpz_DU019A"]
 
-interest_tif = [x for x in tif_files if x.split(
-    '/')[-1].split('.')[0] in interest_rivers]
+interest_tif = [x for x in projected_tif if os.path.basename(x).split(".")[
+    0] in interest_rivers]
+
+
+def extract_zones(tif_file, zone_file, zone, expression):
+    out_file = tif_file.replace('.tif', f'_{zone}.tif')
+    calculator(tif_file, out_file,
+               f'"{os.path.basename(tif_file).split(".")[0]}@1" {expression}')
+    reprojected_out = out_file.replace(".tif", "_reprojected.tif")
+    reproject(out_file, reprojected_out)
+    zonal_stat(reprojected_out, zone_file,
+               out_file.replace('.tif', '_stats.gpkg'))
+
 
 # extract three layers and compute their stats
-for i, tif in enumerate(interest_tif):
-    zone_file = "./data/BioClim/2000/bio1.tif"
-    out_urban = tif.replace('.tif', '_urban.tif')
-    calculator(tif, out_urban, f'"{interest_rivers[i]}@1" = 1')
-    zonal_stat(out_urban, zone_file, out_urban.replace('.tif', '_stats.gpkg'))
-    out_crop = tif.replace('.tif', '_crop.tif')
-    calculator(tif, out_crop, f'"{interest_rivers[i]}@1" = 2')
-    zonal_stat(out_crop, zone_file, out_crop.replace('.tif', '_stats.gpkg'))
-    out_land = tif.replace('.tif', '_land.tif')
-    calculator(tif, out_land, f'"{interest_rivers[i]}@1" >= 3')
-    zonal_stat(out_land, zone_file, out_land.replace('.tif', '_stats.gpkg'))
+for tif in interest_tif:
+    zone_file = "./data/rivers/riparian_zones/france_tiles.tif"
+    extract_zones(tif, zone_file, "urban", "= 1")
+    extract_zones(tif, zone_file, "crop", "= 2")
+    extract_zones(tif, zone_file, "land", ">= 3")
 
 
 '''
